8acecadb259477e3e6461ed6f666bfaf11e18143
Sai Kumar <60894063+sai2729@users.noreply.github.com>
2020-03-30 13:56:06 +0530

    Updated code
    Code updated --- No errors while fetching the result of Detained rollno

--- firstpro.py
+++ firstpro.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import time
 
-#browser = webdriver.Chrome(executable_path='C:\webdrivers\chromedriver.exe')
+browser = webdriver.Chrome(executable_path='C:\webdrivers\chromedriver.exe')
 
-hallticketno="179F1A0500"
-for i in range(0,15):
+hallticketno="179F1A0520"
+for i in range(0,4):
     num=hallticketno[7:]
     num=int(num)+1
     hallticketno=hallticketno[0:len(hallticketno)-3]+str(num)
@@ -17,5 +18,12 @@ for i in range(0,15):
     hallticket=browser.find_element_by_id('ht')
     hallticket.send_keys(hallticketno)
     search.click()
-    time.sleep(3)
+
+    try:
+        browser.implicitly_wait(10)
+        browser.find_element_by_xpath('/html/body/div/div[1]/div/div/center/div[1]/table')
+    except NoSuchElementException:
+        print('Fail')
+        continue
+    time.sleep(1)
     print('pass')
