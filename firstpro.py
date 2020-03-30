c44e79c089c44ac575bcd1fe05a20754d0a66e6c
Sai Kumar <60894063+sai2729@users.noreply.github.com>
2020-03-30 15:03:08 +0530

    Getting marks into terminal
    Now all marks are getting from website to the terminal

--- firstpro.py
+++ firstpro.py
@@ -6,7 +6,7 @@ import time
 browser = webdriver.Chrome(executable_path='C:\webdrivers\chromedriver.exe')
 
 hallticketno="179F1A0520"
-for i in range(0,4):
+for i in range(0,3):
     num=hallticketno[7:]
     num=int(num)+1
     hallticketno=hallticketno[0:len(hallticketno)-3]+str(num)
@@ -23,7 +23,15 @@ for i in range(0,4):
         browser.implicitly_wait(10)
         browser.find_element_by_xpath('/html/body/div/div[1]/div/div/center/div[1]/table')
     except NoSuchElementException:
-        print('Fail')
+        print('Student Details Not Found')
         continue
+
+    rows=len(browser.find_elements_by_xpath('//*[@id="rs"]/table/tbody/tr'))
+    cols=len(browser.find_elements_by_xpath('//*[@id="rs"]/table/tbody/tr[1]/th'))
+    
+    for r in range(2,rows):
+        for c in range(1,cols+1):
+            value=browser.find_element_by_xpath("//*[@id='rs']/table/tbody/tr["+str(r)+"]/td["+str(c)+"]").text
+            print(value,end=' ')
+        print('')
     time.sleep(1)
-    print('pass')
